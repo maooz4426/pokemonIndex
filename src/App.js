8713758bd5dc578afc4b12bff2975f75000968bc
maooz4426 <maooz4426@gmail.com>
2023-08-31 23:33:25 +0900

    2回目

--- src/App.js
+++ src/App.js
@@ -11,19 +11,21 @@
            fetch("https://poke-iota-ten.vercel.app/api/pokedex")
              .then(response => response.json())
              .then(data => {
+              // 初回表示用に作製
               const allPokemon = data.items;
                  data.items.forEach(pokemon => {
-                  
                     // 検索ボタンがクリックされたときの処理
                     searchButton.addEventListener("click", () => {
                       const searchTerm = searchInput.value.toLowerCase();
-                      console.log(searchTerm);
-                      const filteredPokemon = allPokemon.filter(pokemon =>
+                      
+                      const filteredPokemon = pokemon.filter(pokemon =>
+                        
                           pokemon.name.toLowerCase().includes(searchTerm)
                       );
 
-                      pokemonListElement.innerHTML = ""; // リストをクリア
+                      // pokemonListElement.innerHTML = ""; // リストをクリア
                       filteredPokemon.forEach(pokemon => {
+                        pokemonListElement.innerHTML = ""; // リストをクリア
                           const listItem = document.createElement('div');
                           listItem.className = "pokemonItem";
                           const pokemonNo = document.createElement('p');
@@ -43,9 +45,12 @@
                           pokemonListElement.appendChild(listItem);
                       });
                   });
+                });
 
+                  
                   // 初回表示
                   allPokemon.forEach(pokemon => {
+                    console.log(pokemon)
                     const listItem = document.createElement('div');
                     listItem.className = "pokemonItem";
                     const pokemonNo = document.createElement('ul');
@@ -56,24 +61,22 @@
                     pokemonName.textContent = pokemon.name;
                     listItem.appendChild(pokemonNo);
                     listItem.appendChild(pokemonName);
+                    // クリックしたら対応する内容を表示
                     listItem.addEventListener('click',() =>{
                       const pokemonNo = pokemon.no;
-                      // console.log(pokemonNo);
                       const pokemonUrl = `/detail.html?no=${pokemonNo}`
                       window.location.href = pokemonUrl;
                     })
-                    // const listItem = document.createElement("ul");
-                    // listItem.textContent = `${pokemon.no}: ${pokemon.name}`;
-                    // console.log(listItem.textContent);
-                    pokemonListElement.appendChild(listItem);
-                  
-                    // console.log(pokemonListElement);
+ 
+                    pokemonListElement.appendChild(listItem);     
                  });               
-                });
+               
               })
              .catch(error => {
+              // apiの取得に失敗したら表示
                console.error("Error fetching data:", error);
-             });}else{
+             });
+            }else{
               console.error(".pokemonList element not found.");
              }
        };
